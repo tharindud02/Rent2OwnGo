641916eab297f12a0085a7a2bddbb82ecb1961a1
Tharindu Damith <tharindud02@gmail.com>
2024-06-26 02:15:28 +0530

    Meta Data Updated

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -37,8 +37,8 @@ const MyApp = ({ Component, ...rest }) => {
   return (
     <Fragment>
       <Head>
-        <title>Rent2OwnGo - Real Estate NextJs Template</title>
-        <meta name="description" content="Rent2OwnGo - Real Estate Nextjs Template" />
+        <title>Rent2OwnGo - Best Houese Renting Web</title>
+        <meta name="description" content="Rent2OwnGo - Best Houese Renting Web" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
