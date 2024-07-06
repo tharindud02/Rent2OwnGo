d180ae01b963a8eb13dc1f7a79b7737b1339f0b2
Tharindu Damith <tharindud02@gmail.com>
2024-07-06 08:10:43 +0530

    remove env data

--- src/lib/ipinfo.js
+++ src/lib/ipinfo.js
@@ -1,13 +1,14 @@
 export const getLocationData = async () => {
+  const ipInforToke = process.env.IPINFOTOKEN;
   try {
-    // Check if the app is running on localhost
-    if (window.location.hostname === 'localhost') {
-      console.log('Running on localhost, skipping location API call.');
-      // Return a default location or handle it as needed
-      return { region: 'Western Province' };
-    }
+    // // Check if the app is running on localhost
+    // if (window.location.hostname === 'localhost') {
+    //   console.log('Running on localhost, skipping location API call.');
+    //   // Return a default location or handle it as needed
+    //   return { region: 'Western Province' };
+    // }
 
-    const response = await fetch(`https://ipinfo.io?token=${process.env.IPINFOTOKEN}`);
+    const response = await fetch(`https://ipinfo.io?token=fc02cb6b5c8c26`);
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
